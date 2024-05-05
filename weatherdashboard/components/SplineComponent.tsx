2eb7a12609d3d59709594a25694b20cf4998cb5d
maherhms <maher.3000@outlook.fr>
2024-05-05 19:21:27 +0200

    Implement Weather Dashboard from IOT with authentication and data read through thingsboard

---
+++ weatherdashboard/components/SplineComponent.tsx
@@ -0,0 +1,10 @@
+'use client'
+import Spline from "@splinetool/react-spline";
+
+export default function SplineComponent() {
+  return (
+    <div className="h-screen">
+      <Spline scene="https://prod.spline.design/PXZdwIU4jTir4N2G/scene.splinecode" />
+    </div>
+  );
+}
